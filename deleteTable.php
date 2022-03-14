5d6fbd9e48195c7714b6908d4937c2eca36074e4
Quentin Andreani <82041048+qandreani@users.noreply.github.com>
2022-03-14 08:18:28 +0100

    Delete deleteTable.php

--- deleteTable.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('config.php');
-
-$userReq = $pdo->prepare("DELETE FROM `user` WHERE `id` = 4;");
-//$userReq = $pdo->prepare("TRUNCATE TABLE `utilisateur`");
-$userReq->execute();
